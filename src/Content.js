6f1316f2d954c585058c312da5c119b1d4c974cb
Dagmar Lewis <dagmar@dagmar.dev>
2022-04-17 12:06:39 -0500

    made onclick events

--- src/Content.js
+++ src/Content.js
@@ -6,9 +6,23 @@ function Content() {
         const int = Math.floor(Math.random() * 3)
         return names[int]
     }
+    const handleClick = () => {
+        console.log('you clicked it')
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name}you clicked it`)
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
+
     return (
         <main>
             <p>Hello {handleNameChange()}</p>
+            <button onClick={(handleClick)}>Click it</button>
+            <button onClick={(()=> handleClick2('Dagmar'))}>Click it</button>
+            <button onClick={((e)=> handleClick3(e))}>Click it</button>
         </main>
     )
 }
