f6b0c7c40df5fcef91204d71f1789c6e13e85d7b
Dagmar Lewis <dagmarlewis8@gmail.com>
2022-04-20 21:02:48 -0500

    created react list

--- src/Content.js
+++ src/Content.js
@@ -1,29 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
 
-function Content() {
-    const handleNameChange = () => {
-        const names = ['dark', 'bark', 'lark']
-        const int = Math.floor(Math.random() * 3)
-        return names[int]
-    }
-    const handleClick = () => {
-        console.log('you clicked it')
-    }
-    const handleClick2 = (name) => {
-        console.log(`${name}you clicked it`)
-    }
-
-    const handleClick3 = (e) => {
-        console.log(e.target.innerText)
-    }
+const Content = () => {
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: true,
+            item: 'One half pound bag of Cocoa Covered Almonds Unsalted',
+        },
+        {
+            id: 2,
+            checked: false,
+            item: 'Item 2',
+        },
+        {
+            id: 3,
+            checked: false,
+            item: 'Item 3',
+        },
+    ])
 
     return (
         <main>
-            <h1>Dagmar Lewis</h1>
-            <p>Hello {handleNameChange()}</p>
-            <button onClick={(handleClick)}>Click it</button>
-            <button onClick={(()=> handleClick2('Dagmar'))}>Click it</button>
-            <button onClick={((e)=> handleClick3(e))}>Click it</button>
+            <ul>
+                {items.map((item) => (
+                    <li className="item" key={item.id}>
+                        <input type="checkbox" checked={item.checked} />
+                        <label>{item.item}</label>
+                        <button>Delete</button>
+                    </li>
+                ))}
+            </ul>
         </main>
     )
 }
